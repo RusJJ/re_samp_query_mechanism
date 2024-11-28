148826274f8db971392069c623059f109ef7884b
[-=KILL MAN=-] <mctesting1@yandex.ru>
2024-11-28 19:14:46 +0300

    Revert Update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,4 +6,3 @@ LOCAL_MODULE := squery
 LOCAL_SRC_FILES := main.cpp jni_interface.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
 include $(BUILD_SHARED_LIBRARY)
-include $(BUILD_STATIC_LIBRARY)

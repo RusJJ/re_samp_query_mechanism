0f59e2d605bbb99f1563491909ce7caf5e0bff58
[-=KILL MAN=-] <mctesting1@yandex.ru>
2024-11-29 03:55:09 +0300

    Missing stuff from info query

--- jni_interface.cpp
+++ jni_interface.cpp
@@ -44,4 +44,28 @@ JNIEXPORT jint JNICALL Java_net_rusjj_sampquery_NativeInterface_getMaxPlayers(JN
     return s_query->max_players;
 }
 
+JNIEXPORT jstring JNICALL Java_net_rusjj_sampquery_NativeInterface_getHostname(JNIEnv* env, jobject thiz, jlong query)
+{
+    if(!query) return env->NewStringUTF("Unresolved server...");
+    s_server_info* s_query = (s_server_info*)query;
+    if(s_query->type != QUERYTYPE_SERVERINFO) env->NewStringUTF("Unresolved server...");
+    return env->NewStringUTF(s_query->host_name.c_str());
+}
+
+JNIEXPORT jstring JNICALL Java_net_rusjj_sampquery_NativeInterface_getGamemode(JNIEnv* env, jobject thiz, jlong query)
+{
+    if(!query) return env->NewStringUTF("-");
+    s_server_info* s_query = (s_server_info*)query;
+    if(s_query->type != QUERYTYPE_SERVERINFO) env->NewStringUTF("-");
+    return env->NewStringUTF(s_query->game_mode.c_str());
+}
+
+JNIEXPORT jstring JNICALL Java_net_rusjj_sampquery_NativeInterface_getLanguage(JNIEnv* env, jobject thiz, jlong query)
+{
+    if(!query) return env->NewStringUTF("-");
+    s_server_info* s_query = (s_server_info*)query;
+    if(s_query->type != QUERYTYPE_SERVERINFO) env->NewStringUTF("-");
+    return env->NewStringUTF(s_query->language.c_str());
+}
+
 };

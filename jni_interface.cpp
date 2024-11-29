e857d985208595664aacd080dfa73d16c42d44b5
[-=KILL MAN=-] <mctesting1@yandex.ru>
2024-11-29 03:08:29 +0300

    Update jni_interface.cpp
    Just some basic JNI code for tests

--- jni_interface.cpp
+++ jni_interface.cpp
@@ -1,3 +1,47 @@
 #include <jni.h>
 #include "sqm.hpp"
 
+extern "C" {
+    
+JNIEXPORT jobject JNICALL Java_net_rusjj_sampquery_NativeInterface_doQuery(JNIEnv* env, jobject thiz, jint type, jstring ip, jint port)
+{
+    jobject ret = NULL;
+    const char* ipPtr = env->GetStringUTFChars(ip, NULL);
+    
+    auto sqm = c_sqm::singleton();
+	sqm->initialize(ipPtr, port);
+    if(type == QUERYTYPE_SERVERINFO)
+    {
+        ret = (jobject)( sqm->handleInst<s_server_info>(sqm->query(QUERYTYPE_SERVERINFO, 1)) );
+    }
+    sqm->shutdown();
+    return ret;
+}
+
+JNIEXPORT void JNICALL Java_net_rusjj_sampquery_NativeInterface_cleanQuery(JNIEnv* env, jobject thiz, jobject query)
+{
+    if(!query) return;
+    s_query_base* query_base = (s_query_base*)query;
+    if(query_base->type == QUERYTYPE_SERVERINFO)
+    {
+        delete ((s_server_info*)query);
+    }
+}
+
+JNIEXPORT jint JNICALL Java_net_rusjj_sampquery_NativeInterface_getPlayers(JNIEnv* env, jobject thiz, jobject query)
+{
+    if(!query) return 0;
+    s_server_info* s_query = (s_server_info*)query;
+    if(s_query->type != QUERYTYPE_SERVERINFO) return 0;
+    return s_query->players;
+}
+
+JNIEXPORT jint JNICALL Java_net_rusjj_sampquery_NativeInterface_getMaxPlayers(JNIEnv* env, jobject thiz, jobject query)
+{
+    if(!query) return 0;
+    s_server_info* s_query = (s_server_info*)query;
+    if(s_query->type != QUERYTYPE_SERVERINFO) return 0;
+    return s_query->max_players;
+}
+
+};
